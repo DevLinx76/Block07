9d44773bc5e23a62439aa0d21c42c82974e5f658
DevLinx76 <126533274+DevLinx76@users.noreply.github.com>
2023-09-01 19:50:03 -0400

    Edited in math

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-// Pseudocode:
-// 1. Calculate the first number as 10 plus 30.
-// 2. Calculate the second number as 40 divided by 2.
-// 3. Calculate the third number as 50 minus 11.
-// 4. Display the combination on the HTML page.
-// 5. Display an alert with the combination.
+// Step 1: Create a variable for the first number (10).
+const number1 = 5 * 2; // Multiplication to get 10
 
-// Calculation steps
-const firstNumber = 10 + 30;  // Step 1
-const secondNumber = 40 / 2;  // Step 2
-const thirdNumber = 50 - 11;  // Step 3
+// Step 2: Create a variable for the second number (40).
+const number2 = 100 - 60; // Subtraction to get 40
 
-// Display the combination on the HTML page
-const combinationElement = document.getElementById("combination");
-combinationElement.textContent = `${firstNumber} - ${secondNumber} - ${thirdNumber}`;
+// Step 3: Create a variable for the third number (39).
+const number3 = 80 / 2 + 19; // Division and addition to get 39
 
-// Display an alert with the combination
-alert(`Vault Combination: ${firstNumber} - ${secondNumber} - ${thirdNumber}`);
+// Combine the numbers with hyphens to form the combination.
+const combination = `${number1} - ${number2} - ${number3}`;
+
+// Display the combination in an alert popup.
+alert(`The combination is: ${combination}`);
+
