9d4df5f21bb2c181ead073577b73023e1a23f916
DevLinx76 <126533274+DevLinx76@users.noreply.github.com>
2023-08-31 20:30:12 -0400

    Secure the Vault

---
+++ script.js
@@ -0,0 +1,13 @@
+// Calculate the combination numbers
+const firstNumber = 10 + 30;  // Addition operator
+const secondNumber = 40 / 2;  // Division operator
+const thirdNumber = 50 - 11;  // Subtraction operator
+
+// Display the combination on the HTML page
+const combinationElement = document.getElementById("combination");
+combinationElement.textContent = `${firstNumber} - ${secondNumber} - ${thirdNumber}`;
+
+// Display the combination in an alert popup
+alert(`Vault Combination: ${firstNumber} - ${secondNumber} - ${thirdNumber}`);
+
+
