523b77e1bebd339a3854ae54bdf9e8e3404b41d1
DevLinx76 <126533274+DevLinx76@users.noreply.github.com>
2023-09-01 20:24:13 -0400

    Edited at Setp 3

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const number1 = 5 * 2; // Multiplication to get 10
 const number2 = 100 - 60; // Subtraction to get 40
 
 // Step 3: Create a variable for the third number (39).
-const number3 = 80 / 2 + 19; // Division and addition to get 39
+const number3 = (20 - 10) + 29; // Subtraction and addition to get 39
 
 // Combine the numbers with hyphens to form the combination.
 const combination = `${number1} - ${number2} - ${number3}`;
