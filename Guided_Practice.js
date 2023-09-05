7e4d94a85376caa2e1863d39f20578608c5f4149
DevLinx76 <126533274+DevLinx76@users.noreply.github.com>
2023-09-05 02:31:19 -0400

    Guided_Practice

---
+++ Guided_Practice.js
@@ -0,0 +1,60 @@
+//Pseudocode and JavaScript Practice
+
+/* Open your IDE and complete the following problems. 
+Remember to try them on your own first, and then check your answer. */
+
+/* 1. In eight steps or more, use pseudocode to explain the steps 
+of brushing your teeth. 
+
+1. Begin
+2. Stand in front of the bathroom sink.
+3. Pick up the toothbrush.
+4. Turn on the tap to wet the toothbrush bristles.
+5. Apply toothpaste onto the wet toothbrush bristles.
+6. Turn off the tap to conserve water.
+7. Lift the toothbrush to mouth.
+8. Open mouth.
+9. Place the toothbrush against teeth.
+10. Brush teeth in a circular motion.
+11. Ensure brush all surfaces of teeth: front, back, top, and chewing surfaces.
+12. Continue brushing for at least two minutes.
+13. Spit out the toothpaste and saliva into the sink.
+14. Rinse mouth with water.
+15. Close mouth.
+16. Rinse the toothbrush thoroughly under running water.
+17. Turn off the tap.
+18. Replace the toothbrush in its holder or container. 
+19. Smile at yourself in the mirror (optional).
+20. Finish
+*/
+
+// 2. Declare a variable for the number of cows and assign it a value of 50.
+let numberOfCows = 50;
+
+// 3. Create a string that says Hello World.
+let myString = 'Hello World';
+
+// 4. Display Hello World in the console log. Hint: Open the console view before running.
+console.log(Hello World!);
+
+// 5. Display a dialog box that says Hello World.
+alert('Hello World!');
+
+// 6. Create a variable for the following with the arithmetic operator: the number of umbrellas is ten times three. 
+let numberOfUmbrellas = 10 * 3;
+
+// 7. Create a variable num with the addition operator of five plus one. Then, show the value through an alert.
+let num = 5 + 1;
+alert(num);
+
+// 8. Add an external script file to an HTML file.
+<script src="script.js"></script>
+
+// 9. Concatenate three strings (1) Hello (2) [space] (3) World.
+let myString = 'Hello' + ' ' + 'World';
+
+// 10. Concatenate strings using variables (1) Hello, (2) [space] (3) I am (4) [space] (5) Josh. Display over a dialog box.
+let myString = 'Hello';
+let myString2 = 'I am';
+let myString3 = 'Josh';
+alert(myString + ' ' + myString2 + ' ' + myString3);
